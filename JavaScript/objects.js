4848dca9ed2251bb7fe4b82e25ee2c3ae460e9be
Ant2965 <antrikshbahri@gmail.com>
2024-01-10 12:14:09 +0530

    js

---
+++ JavaScript/objects.js
@@ -0,0 +1,15 @@
+let arr =[];
+typeof arr;
+
+
+let delhi = {
+    lati: "28.70 N",
+    long: "77.1025 E"
+};
+
+const student ={
+    name: "shradha",
+    age: 23,
+    marks: 94.4,
+    city: "Delhi"
+};
