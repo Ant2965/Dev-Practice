162933ae45ed520d936c1075f1120630aa64c7cd
Ant2965 <antrikshbahri@gmail.com>
2024-01-13 18:43:04 +0530

    EVERY and Some METHODS

--- JavaScript/array.js
+++ JavaScript/array.js
@@ -44,3 +44,24 @@ let gpa = obj.map((el)=>{
     return el.marks/10;
 })
 
+///           FILTERR   FUNCTION
+
+
+let nums=[1,2,3,4,5,6,7,8,9,0];
+let ans =nums.filter((el)=>{
+    return el%2==0;
+})
+
+
+//   EVERY METHOD
+
+// returns only teue and false      Logic AND operator
+
+let arrr=[2,4,6,8,67].every((el)=>{
+    return el%2==0;
+})
+
+
+
+//    SOME FUNCTION    Logical OR operator
+
