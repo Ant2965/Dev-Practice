9f810d6de141a008f1a400f473d1c42bae923f1c
Ant2965 <antrikshbahri@gmail.com>
2024-01-23 19:50:46 +0530

    get attribute and set attribute

--- dom/app.js
+++ dom/app.js
@@ -14,3 +14,14 @@ img.setAttribute('id','antman')
 //undefined
 img.id
 //'//antman'
+
+
+let links= document.querySelectorAll('.box a');
+
+for(let i =0;i<links.length;i++){
+    links[i].style.color='purple'
+}
+
+for(link of links){
+    link.style.backgroundColor='red';
+}
