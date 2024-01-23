71b8907cf332d80f108e99e554559965c95070b8
Ant2965 <antrikshbahri@gmail.com>
2024-01-23 19:33:59 +0530

    get attribute and set attribute

--- dom/app.js
+++ dom/app.js
@@ -3,3 +3,14 @@ let si = document.getElementsByClassName("oldImg");
 for(let i=0;i<si.length;i++){
     console.dir(si[i]);
 }
+
+// changing the attributes
+let img = document.querySelector('img')
+img.getAttribute('id')
+//'mainImg'
+img.getAttribute('src')
+//'assets/spiderman_img.png'
+img.setAttribute('id','antman')
+//undefined
+img.id
+//'//antman'
