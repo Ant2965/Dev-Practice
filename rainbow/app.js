ce2eeb8f2c84cbbcc08b3abcb104eae4fc391172
Ant2965 <antrikshbahri@gmail.com>
2024-01-26 14:33:14 +0530

    DOM Events

---
+++ rainbow/app.js
@@ -0,0 +1,38 @@
+let b1=document.querySelector('.v');
+let b2=document.querySelector('.i');
+let b3=document.querySelector('.b');
+let b4=document.querySelector('.g');
+let b5=document.querySelector('.y');
+let b6=document.querySelector('.o');
+let b7=document.querySelector('.r');
+
+
+
+
+b1.onclick=()=>{
+    document.querySelector("body").style.backgroundColor='purple'
+}
+
+b2.onclick=()=>{
+    document.querySelector("body").style.backgroundColor='indigo'
+}
+
+b3.onclick=()=>{
+    document.querySelector("body").style.backgroundColor='blue'
+}
+
+b4.onclick=()=>{
+    document.querySelector("body").style.backgroundColor='green'
+}
+
+b5.onclick=()=>{
+    document.querySelector("body").style.backgroundColor='yellow'
+}
+
+b6.onclick=()=>{
+    document.querySelector("body").style.backgroundColor='orange'
+}
+
+b7.onclick=()=>{
+    document.querySelector("body").style.backgroundColor='red'
+}
