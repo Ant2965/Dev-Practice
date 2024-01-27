7db36e9b967fefbb1daaf29f65dc8f60ed5e36ca
Ant2965 <antrikshbahri@gmail.com>
2024-01-27 10:27:22 +0530

    DOM Events js form and event listeners

--- rainbow/form.js
+++ rainbow/form.js
@@ -4,4 +4,17 @@ let form= document.querySelector("form")
 form.addEventListener("submit", ()=>{
     event.preventDefault();
     alert("Form submitted");
+
+
+//let user=document.querySelector("#user");
+let user=this.Element[0];
+let pass =this.Element[1];
+//let pass=document.querySelector("#password");
+
+console.dir(user.value);
+console.log(pass.value);
+
+
+
+alert(`Hi ${user.value}, your password is ${pass.value}`);
 })
